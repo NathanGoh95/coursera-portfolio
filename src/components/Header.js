92e43b607c5f30de611300964272bab5b1e2f42f
Nathan Goh <gohjunhao95@gmail.com>
2023-10-29 22:38:19 +0800

    Initial commit + navbar set

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from '@fortawesome/free-brands-svg-icons';
+import { Box, HStack } from '@chakra-ui/react';
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: 'mailto: gohjunhao95@gmail.com',
+  },
+  {
+    icon: faGithub,
+    url: 'https://github.com',
+  },
+  {
+    icon: faLinkedin,
+    url: 'https://www.linkedin.com',
+  },
+  {
+    icon: faMedium,
+    url: 'https://medium.com',
+  },
+  {
+    icon: faStackOverflow,
+    url: 'https://stackoverflow.com',
+  },
+];
+
+const Header = () => {
+  const handleClick = anchor => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            {/* Add social media links based on the `socials` data */}
+            <HStack direction={['column', 'row']} spacing={5}>
+              {socials.map((socials, index) => (
+                <a
+                  key={index}
+                  href="{socials.url}"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={socials.icon} size="xl" />
+                </a>
+              ))}
+            </HStack>
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              {/* Add links to Projects and Contact me section */}
+              <a
+                href="#projects-section"
+                onClick={handleClick('projects-section')}
+              >
+                Projects
+              </a>
+              <a
+                href="#contactme-section"
+                onClick={handleClick('contactme-section')}
+              >
+                Contact Me
+              </a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
